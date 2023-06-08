d466e7f58054a5271290a604e029ab7b1bb94348
Dilber KILIÇ <48033000+dilberkilic@users.noreply.github.com>
2023-06-08 13:50:12 +0300

    Update
    Day-8

--- 8-Day.playground/Contents.swift
+++ 8-Day.playground/Contents.swift
@@ -1,3 +1,26 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//-------Creating your own structs-------
+struct Sport1 {
+    var name: String
+}
+var tennis = Sport1(name: "Tennis")
+print(tennis.name)
+tennis.name = "Lawn tennis"
+
+//------Computed properties-------
+struct Sport {
+    var name: String
+    var isOlympicSport: Bool
+
+    var olympicStatus: String {
+        if isOlympicSport {
+            return "\(name) is an Olympic sport"
+        } else {
+            return "\(name) is not an Olympic sport"
+        }
+    }
+}
+
+let chessBoxing = Sport(name: "Chessboxing", isOlympicSport: false)
+print(chessBoxing.olympicStatus)
