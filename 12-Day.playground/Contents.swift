1929d0e020de67dbe4540d271efea12e60bdaa3f
Dilber KILIÇ <48033000+dilberkilic@users.noreply.github.com>
2023-06-19 23:16:40 +0300

    update day 12

--- 12-Day.playground/Contents.swift
+++ 12-Day.playground/Contents.swift
@@ -41,3 +41,52 @@ if let doubled = double(number: input) {
     print("\(input) doubled is \(doubled).")
 }
 
+//----------Force unwrapping-----------
+
+//let num = Int(str)!
+//boş gelmiyeceğinden emin olduğumuz kısımlara koyarız.
+
+//----------Implicitly unwrapped optionals---------
+
+//let age: Int! = nil
+
+//---------Nil coalescing---------
+//let user = username(for: 15) ?? "Anonymous"
+
+//-----Optional chaining--------
+let names = ["John", "Paul", "George", "Ringo"]
+let beatle = names.first?.uppercased()
+
+//---------Optional try-------
+
+enum PasswordError: Error {
+    case obvious
+}
+
+func checkPassword(_ password: String) throws -> Bool {
+    if password == "password" {
+        throw PasswordError.obvious
+    }
+
+    return true
+}
+
+do {
+    try checkPassword("password")
+    print("That password is good!")
+} catch {
+    print("You can't use that password.")
+}
+
+//hata olmayacağına eminsek
+try! checkPassword("sekrit")
+print("OK!")
+
+//hatanın ne olduğu ile ililenmiyorsak.
+/*if let result = try? runRiskyFunction() {
+    print(result)
+}*/
+
+//-------------Failable initializers--------
+
+
