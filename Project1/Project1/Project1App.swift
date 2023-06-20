d137b138cb929e829e14136e5e2fe3d89fa06f42
Dilber KILIÇ <48033000+dilberkilic@users.noreply.github.com>
2023-06-20 09:57:42 +0300

    Project1 create

---
+++ Project1/Project1/Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1App.swift
+//  Project1
+//
+//  Created by Dilber KILIÇ on 19.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
