c5a4982afabe811669000a61abe5022e16a04470
Dilber KILIÇ <48033000+dilberkilic@users.noreply.github.com>
2023-06-21 22:50:03 +0300

    Delete project-1 and create project1-2
    Delete project-1 and create project1-2

---
+++ Project-1-2/Project-1-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project-1-2
+//
+//  Created by Dilber KILIÇ on 21.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
